251e3b4684ae469cfcfa0d4bc4c4ef0e88d789ba
Volkov Aleksei <rohtuasad@gmail.com>
2021-09-14 23:31:02 +0300

    Initialize Spring Web application

---
+++ src/test/kotlin/ru/rohtuasad/todoapp/ToDoAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.rohtuasad.todoapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
